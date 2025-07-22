a8c453bb679d333cfc531a19324a18a223cdbd65
David Ntumba <david.ntumba@smollan.com>
2025-07-23 00:06:56 +0200

    20250723 tinker: looking into registering commands

--- cloud-storage-explorer/src/extension.ts
+++ cloud-storage-explorer/src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cloud-storage-explorer" is now active!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('cloud-storage-explorer.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from Cloud Storage Explorer!');
+		}));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cloud-storage-explorer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Cloud Storage Explorer!');
-	});
-
-	context.subscriptions.push(disposable);
+	// Register Goodbye World command
+	context.subscriptions.push(
+		vscode.commands.registerCommand('cloud-storage-explorer.goodbyeWorld', () => {
+			vscode.window.showInformationMessage('Goodbye World from Cloud Storage Explorer!');
+		}));
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
